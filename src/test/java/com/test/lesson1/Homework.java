c4e11f6f6e941116fc89382e552c1801bcb68264
akramyakhyaev <alexsamedov1@gmail.com>
2023-07-03 16:31:59 -0500

    test

---
+++ src/test/java/com/test/lesson1/Homework.java
@@ -0,0 +1,10 @@
+package com.test.lesson1;
+
+public class Homework {
+    public static void main(String[] args) {
+
+    }
+    private static void task2(){
+        System.out.println("hello world");
+    }
+}
